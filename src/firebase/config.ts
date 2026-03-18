861da651a31280181567d3626c1b1758b7b87569
15M views <machariastanely@gmail.com>
2026-03-18 12:42:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-knvm3",
+  "appId": "1:355598952899:web:3434b2aed77585cd23b576",
+  "apiKey": "AIzaSyBtAPMSUUidBFxdy2sievokomI9RiQCMJ8",
+  "authDomain": "studio-knvm3.firebaseapp.com",
+  "measurementId": "G-7YKXRRRHY1",
+  "messagingSenderId": "355598952899"
+};
